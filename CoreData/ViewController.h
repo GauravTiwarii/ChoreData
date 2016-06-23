611aca186645e85174f8b4977206810301e6dded
Love <Love@love.local>
2016-06-24 02:42:27 +0530

    Initial Commit

---
+++ CoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreData
+//
+//  Created by Love on 6/24/16.
+//  Copyright © 2016 Love. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
